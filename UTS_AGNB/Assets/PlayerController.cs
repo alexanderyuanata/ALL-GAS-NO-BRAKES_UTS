7a0cd852f2678621fa0ea76f683f242bc8730081
unfocusedlex <92197888+unfocusedlex@users.noreply.github.com>
2023-03-13 21:16:46 +0700

    basic movement

--- UTS_AGNB/Assets/PlayerController.cs
+++ UTS_AGNB/Assets/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Global variables
+    public float movement_speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        transform.Translate(direction * movement_speed * Time.deltaTime);
     }
 }
